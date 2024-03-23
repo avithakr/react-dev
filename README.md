3e083900843a93ccf82c2cecbcefe3b35e737c0a
Avinash Thakur <thakuravisuman@gmail.com>
2024-03-23 12:05:56 +0530

    episode-02

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # React development
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in c++
+- Caching - Faster Build
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-const parent = React.createElement(
-  'div',
-  { id: 'parent' },
-  React.createElement('div', { id: 'child' }, [
-    React.createElement(
-      'h1',
-      { id: 'heading', key: 'heading' },
-      'Hey I Am H1 Tag...!'
-    ),
-    React.createElement('p', { key: 'para' }, 'lorem .....'),
-  ])
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const count = 1112;
+
+const Title = () => {
+  return (
+    <div>
+      <h2>This is title component</h2>
+      <span>Count : </span> <span>{count}</span> <br />
+      <br />
+    </div>
+  );
+};
+
+const HeadingComponent = () => (
+  <div>
+    <h1 className="heading">Hey i am from jsx.....</h1>
+    <Title />
+  </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+
+root.render(<HeadingComponent />);
